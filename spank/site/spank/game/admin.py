f70aaa63cd14caa982fa284fd98d4b39d5e814ae
alessio.desanto <alessio.desanto@he-arc.ch>
2013-11-15 10:05:25 +0100

    131115 -- admin interface

--- spank/site/spank/game/admin.py
+++ spank/site/spank/game/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Poll, Choice, Answer, Friend, Visit
+from .models import Poll, Choice, Answer, Friend, Visit, User
 
 admin.site.register(Visit)
+admin.site.register(User)
 admin.site.register(Poll)
 admin.site.register(Choice)
 admin.site.register(Answer)
