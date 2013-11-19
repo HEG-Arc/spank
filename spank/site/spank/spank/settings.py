8ce6394bc609a6add3546a97832d765748a95555
alessio.desanto <alessio.desanto@he-arc.ch>
2013-11-19 17:05:39 +0100

    131119 -- previous/forward

--- spank/site/spank/spank/settings.py
+++ spank/site/spank/spank/settings.py
@@ -35,7 +35,7 @@ DATABASES = {
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'admin',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
