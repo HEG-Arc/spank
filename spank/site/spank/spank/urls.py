44f592e6f8d44eea0e9c476d74b2dd3384e016fd
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2013-11-20 18:05:23 +0100

    Changed js sources from google to local

--- spank/site/spank/spank/urls.py
+++ spank/site/spank/spank/urls.py
@@ -19,4 +19,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name='splash/index.html'), name='index'),
 )
---
+++ spank/site/spank/static/js/main.js
@@ -0,0 +1,2 @@
+viewport = document.querySelector("meta[name=viewport]");
+viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, minimum-scale=1.0');
