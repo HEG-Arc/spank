6bc37e24b56b11dc7b131030ac5c0c1747d77098
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2013-11-23 12:37:40 +0100

    Brilliant admin...

--- spank/site/spank/game/admin.py
+++ spank/site/spank/game/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Poll, Choice, Answer, Friend, Visit, User
 
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'auth_accepted', 'email', 'coupable', 'created_at', 'updated_at')
+    list_filter = ('created_at', )
+
+
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Visit)
-admin.site.register(User)
 admin.site.register(Poll)
 admin.site.register(Choice)
 admin.site.register(Answer)
